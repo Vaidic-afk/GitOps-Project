dbafdb23f141ace8e22bb2fced0e83b521fc0e79
Vaidic Tiwari <vaidictiwari14@outlook.com>
2026-04-30 02:43:06 +0530

    Added .gitignore and edited backend.tf.

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatebaivabremote"
-    container_name       = "tfstate"
-    key                  = "dev.gitops.tfstate"
-    use_azuread_auth     = false
-    use_msi              = true
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "tfstate-rg"
+#     storage_account_name = "tfstatebaivabremote"
+#     container_name       = "tfstate"
+#     key                  = "dev.gitops.tfstate"
+#     use_azuread_auth     = false
+#     use_msi              = true
+#   }
+# }
